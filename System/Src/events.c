9f59d39212d258d94cc2725e490d59a56f1e2599
Terence Kim <terence.kim@apple.com>
2023-07-21 12:04:31 -0700

    Add critical sections to events

--- System/Src/events.c
+++ System/Src/events.c
@@ -5,20 +5,31 @@
  *      Author: terence_kim
  */
 #include "events.h"
+#include "target.h"
 
 static uint32_t evtMask;
 
 void eventSet(event_idx_e eventIdx)
 {
+  ENTER_CRIT_SECTION;
   evtMask |= (1UL << eventIdx);
+  EXIT_CRIT_SECTION;
 }
 
 void eventClear(event_idx_e eventIdx)
 {
+  ENTER_CRIT_SECTION;
   evtMask &= ~(1UL << eventIdx);
+  EXIT_CRIT_SECTION;
 }
 
 bool eventIsSet(event_idx_e eventIdx)
 {
-  return (evtMask & (1UL << eventIdx)) != 0;
+  bool ret;
+
+  ENTER_CRIT_SECTION;
+  ret = (evtMask & (1UL << eventIdx)) != 0;
+  EXIT_CRIT_SECTION;
+
+  return ret;
 }
